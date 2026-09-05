e1596cd1b15c6098cbc9ab645c0388d312cdcfc4
shiva <kotteshivashankar9346@gmail.com>
2026-09-05 15:43:26 +0530

    vite updated

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/',
+  base: '/Personal-Portfolio/',
 
   plugins: [react(), tailwindcss()],
 
